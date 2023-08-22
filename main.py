65046ab3040654fd915d4630f38814e3ef57fbb5
siteflask1 <leandrolimahugo@gmail.com>
2023-08-22 06:02:48 -0300

    update commit

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return "Pinterest"
+
+@app.route("/perfil")
+def perfil():
+    return "Perfil"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
