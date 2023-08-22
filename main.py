e3aee00284be6cd34920fbdb4509900d42e43481
siteflask1 <leandrolimahugo@gmail.com>
2023-08-22 06:15:53 -0300

    update commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Pinterest"
+    return render_template("homepage.html")
 
 @app.route("/perfil")
 def perfil():
