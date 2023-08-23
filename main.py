d2c8a14902323833e723cf0e430a3409461a4dfe
siteflask1 <leandrolimahugo@gmail.com>
2023-08-23 06:42:51 -0300

    update commit

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return "Perfil"
+
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
